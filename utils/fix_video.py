3cb6a72e2c8c6c384785b0c5793e629d6d78f402
kelwinfc <kelwinfc@gmail.com>
2014-06-29 11:22:33 +0100

    Script para arreglar videos

---
+++ utils/fix_video.py
@@ -0,0 +1,11 @@
+import os
+from os import sys
+
+path = sys.argv[1]
+for root, dirs, files in os.walk(path):
+    for idx, f in enumerate(files):
+        v = os.path.join(path, f).replace(' ', '\ ')
+        os.system("rm __0.mpg; ffmpeg -threads 4 -i " + v + \
+                  " -vcodec copy -acodec copy __0.mpg; cp __0.mpg " + v)
+        print "\n\n\n", str(idx+1) +  "/" + str(len(files)), v, "\n\n\n"
+    break
